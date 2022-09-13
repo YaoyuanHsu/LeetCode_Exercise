9551727fee6265250aff56eedad7eb885a1a2cbf
yuanster <124578ster@gmail.com>
2022-09-13 18:39:11 +0800

    寫一半

---
+++ 39.combination-sum.c
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=39 lang=c
+ *
+ * [39] Combination Sum
+ */
+
+// @lc code=start
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+struct node{
+    int remainTarget, now;
+    struct node* back;
+};
+
+typedef struct node NODE;
+
+NODE* newNode(int remainTarget, int now, NODE* back, NODE* reNode){
+    reNode->remainTarget = remainTarget;
+    reNode->now = now;
+    reNode->back = back;
+    return reNode;
+}
+
+int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
+    NODE* reNode;
+    newNode();
+}
+// @lc code=end
+
+/** 
+ * Given an array of distinct integers candidates and a target integer target, 
+ * return a list of all unique combinations of candidates where the chosen numbers sum to target.
+ * You may return the combinations in any order.
+ * The same number may be chosen from candidates an unlimited number of times.
+ * Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+ * The test cases are generated such that the number of unique combinations 
+ * that sum up to target is less than 150 combinations for the given input.
+ * 
+ * Example 1:
+ * Input: candidates = [2,3,6,7], target = 7
+ * Output: [[2,2,3],[7]]
+ * Explanation:
+ * 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+ * 7 is a candidate, and 7 = 7.
+ * These are the only two combinations.
+ * 
+ * Example 2:
+ * Input: candidates = [2,3,5], target = 8
+ * Output: [[2,2,2,2],[2,3,3],[3,5]]
+ * 
+ * Example 3:
+ * Input: candidates = [2], target = 1
+ * Output: []
+ */
