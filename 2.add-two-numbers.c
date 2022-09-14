28c6f2bb311ac18f8154236997be3a3cd1f6d9b9
Yuanster <124578ster@gmail.com>
2022-09-14 19:35:15 +0800

    Reformat code

--- 2.add-two-numbers.c
+++ 2.add-two-numbers.c
@@ -51,28 +51,28 @@
  */
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-        if (!l1)
-                return l2;
-        if (!l2)
-                return l1;
-        int carry = 0;
-        struct ListNode *current, *ans, *last;
-        ans = (struct ListNode*)malloc(sizeof(struct ListNode));
-        current = ans;
-        while (l1 != NULL || l2 != NULL || carry != 0) {
-                current->val = ((l1 != NULL ? l1->val : 0) + (l2 != NULL ? l2->val : 0) + carry) % 10;
-                carry = ((l1 != NULL ? l1->val : 0) + (l2 != NULL ? l2->val : 0) + carry) / 10;
-                if (l1 != NULL)
-                        l1 = l1->next;
-                if (l2 != NULL)
-                        l2 = l2->next;
-                last = current;
-                current->next = (struct ListNode*)malloc(sizeof(struct ListNode));
-                current = current->next;
-        }
-        last->next = NULL;
-        free(current);
-        return ans;
+    if (!l1)
+        return l2;
+    if (!l2)
+        return l1;
+    int carry = 0;
+    struct ListNode *current, *ans, *last;
+    ans = (struct ListNode*)malloc(sizeof(struct ListNode));
+    current = ans;
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        current->val = ((l1 != NULL ? l1->val : 0) + (l2 != NULL ? l2->val : 0) + carry) % 10;
+        carry = ((l1 != NULL ? l1->val : 0) + (l2 != NULL ? l2->val : 0) + carry) / 10;
+        if (l1 != NULL)
+            l1 = l1->next;
+        if (l2 != NULL)
+            l2 = l2->next;
+        last = current;
+        current->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+        current = current->next;
+    }
+    last->next = NULL;
+    free(current);
+    return ans;
 }
 // @lc code=end
 
