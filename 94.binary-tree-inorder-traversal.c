b5f77e6b48df0694b5ad0dc6a68c60de235d3335
yuanster <124578ster@gmail.com>
2022-09-15 14:39:22 +0800

    Easy_94_C
    70/70 cases passed (0 ms)
    Your runtime beats 100 % of c submissions
    Your memory usage beats 66.67 % of c submissions (5.8 MB)

--- 94.binary-tree-inorder-traversal.c
+++ 94.binary-tree-inorder-traversal.c
@@ -17,8 +17,19 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* inorderTraversal(struct TreeNode* root, int* returnSize){
-        *returnSize = 0;
+
+void inorder(int* returnList, struct TreeNode* root, int* returnSize){
+    if(root==NULL) return 0 ;
+    inorder(returnList, root->left, returnSize);
+    returnList[(*returnSize)++] = root->val;
+    inorder(returnList, root->right, returnSize);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    *returnSize = 0;
+    int *returnList = malloc(100 * sizeof(int));
+    inorder(returnList, root, returnSize);
+    return returnList;
 }
 // @lc code=end
 
