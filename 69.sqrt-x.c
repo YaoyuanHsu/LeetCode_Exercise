4c560433a7ca4f6befb7f3ecc420f47fd0b36deb
yuanster <124578ster@gmail.com>
2022-09-22 20:30:18 +0800

    Add commend

--- 69.sqrt-x.c
+++ 69.sqrt-x.c
@@ -10,15 +10,20 @@ int mySqrt(int x) {
     int pre = x / 2, now = x / 4;
     if (x == 1)
         return 1;
+    // 4 is the largest number that (num^2)/4 <= num
     if (x <= 4)
         return x / 2;
     while (1) {
-        if ((now > pre) || (x / pre >= pre))
+        // if search window cross the answer, the upper range is the answer
+        if (x / pre >= pre)
             return pre;
+        // using binary search concept to find the range that sqrt exist
         if (x / now < now || (x / now == now && x % now)) {
             pre = now;
             now /= 2;
-        } else {
+        }
+        // if we find the range search form both ended
+        else {
             pre--;
             now++;
         }
