e7d0f02ec758f9070ef157a8f12dfa139ec66944
yuanster <124578ster@gmail.com>
2022-09-22 23:05:46 +0800

    Easy_557_C
    29/29 cases passed (12 ms)
    Your runtime beats 55.62 % of c submissions
    Your memory usage beats 6.34 % of c submissions (7.7 MB)

--- 557.reverse-words-in-a-string-iii.c
+++ 557.reverse-words-in-a-string-iii.c
@@ -6,7 +6,33 @@
 
 // @lc code=start
 
-char* reverseWords(char* s) {}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* reverseWords(char* s) {
+    int size = 0, length = strlen(s);
+    char *token, *res;
+
+    // initialize length +1(for the last '\0') as '\0'
+    res = (char*)malloc((length + 1) * sizeof(char));
+    memset(res, '\0', length + 1);
+
+    // split string by " "(here can not use ' ')
+    token = strtok(s, " ");
+    while (token != NULL) {
+        // from the position we recorded reverses characters within the length
+        // of return token
+        for (int i = strlen(token) - 1; i >= 0; size++, i--)
+            res[size] = token[i];
+        token = strtok(NULL, " ");
+        // if there still has data in the string, we assign the last character
+        // as space and record plus one
+        if (token)
+            res[size++] = ' ';
+    }
+    return res;
+}
 // @lc code=end
 
 /**
