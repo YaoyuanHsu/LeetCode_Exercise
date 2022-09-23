73208fc80871f99a54de77b4117c4bce4cd93f8c
yuanster <124578ster@gmail.com>
2022-09-23 14:57:44 +0800

    Add new format code
    1032/1032 cases passed (0 ms)
    Your runtime beats 100 % of c submissions
    Your memory usage beats 88.39 % of c submissions (5.4 MB)

--- 7.reverse-integer.c
+++ 7.reverse-integer.c
@@ -11,49 +11,68 @@ int reverse(int x) {
     int* check = malloc(sizeof(int));
     int negative = (x < 0), first = x % 10, remain = x / 10, res = 0;
     while (remain != 0) {
-        // if input is negative number
-        if (negative && INT_MIN / 10 <= res) {
+        if (negative ? (INT_MIN / 10 <= res) : (INT_MAX / 10 >= res)) {
             res *= 10;
-            if (INT_MIN - first <= res)
+            if (negative ? (INT_MIN - first <= res) : (INT_MAX - first >= res))
                 res += first;
             else
                 return 0;
         }
 
-        // if input is positive number
-        else if (!negative && INT_MAX / 10 >= res) {
-            res *= 10;
-            if (INT_MAX - first >= res)
-                res += first;
-            else
-                return 0;
-        } else
+        // // if input is negative number
+        // if (negative && INT_MIN / 10 <= res) {
+        //     res *= 10;
+        //     if (INT_MIN - first <= res)
+        //         res += first;
+        //     else
+        //         return 0;
+        // }
+
+        // // if input is positive number
+        // else if (!negative && INT_MAX / 10 >= res) {
+        //     res *= 10;
+        //     if (INT_MAX - first >= res)
+        //         res += first;
+        //     else
+        //         return 0;
+        // }
+
+        else
             return 0;
+
         first = remain % 10;
         remain = remain / 10;
     }
 
     if (first != 0) {
-        // if input is negative number
-        if (negative && INT_MIN / 10 <= res) {
+        if (negative ? (INT_MIN / 10 <= res) : (INT_MAX / 10 >= res)) {
             res *= 10;
-            if (INT_MIN - first <= res)
+            if (negative ? (INT_MIN - first <= res) : (INT_MAX - first >= res))
                 return res += first;
         }
 
-        // if input is positive number
-        else if (!negative && INT_MAX / 10 >= res) {
-            res *= 10;
-            if (INT_MAX - first >= res)
-                return res += first;
-        } else
+        // // if input is negative number
+        // if (negative && INT_MIN / 10 <= res) {
+        //     res *= 10;
+        //     if (INT_MIN - first <= res)
+        //         return res += first;
+        // }
+
+        // // if input is positive number
+        // else if (!negative && INT_MAX / 10 >= res) {
+        //     res *= 10;
+        //     if (INT_MAX - first >= res)
+        //         return res += first;
+        // }
+
+        else
             return 0;
     }
 
     return res;
 }
 // @lc code=end
-// 1463847412
+
 /**
  * @brief Given a signed 32-bit integer x, return x with its digits reversed.
  * If reversing x causes the value to go outside the signed 32-bit integer
