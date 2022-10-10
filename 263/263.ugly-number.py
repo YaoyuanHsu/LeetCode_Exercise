b5931086402821139f1461b844a310f2dc6a36d2
Yuanster <124578ster@gmail.com>
2022-10-10 17:03:06 +0800

    Another solution
    1013/1013 cases passed (32 ms)
    Your runtime beats 55.34 % of python submissions
    Your memory usage beats 14.5 % of python submissions (13.5 MB)

--- 263/263.ugly-number.py
+++ 263/263.ugly-number.py
@@ -18,18 +18,18 @@ class Solution(object):
     def isUgly(self, n):
         if n <= 0:
             return False
-        tmp = divideBy(n, 2)
-        if tmp == 1:
-            return True
-        tmp = divideBy(tmp, 3)
-        if tmp == 1:
-            return True
-        tmp = divideBy(tmp, 5)
-        if tmp == 1:
-            return  True
-        return False
+        # tmp = divideBy(n, 2)
+        # if tmp == 1:
+        #     return True
+        # tmp = divideBy(tmp, 3)
+        # if tmp == 1:
+        #     return True
+        # tmp = divideBy(tmp, 5)
+        # if tmp == 1:
+        #     return  True
+        # return False
 
-        # return divideBy(divideBy(divideBy(n, 2), 3), 5) == 1
+        return divideBy(divideBy(divideBy(n, 2), 3), 5) == 1
 
 # @lc code=end
 
