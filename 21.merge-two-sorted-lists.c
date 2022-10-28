504e4a48c0b8e69c022db77fd0826c5bf9c8e93b
yuanster <124578ster@gmail.com>
2022-10-28 16:39:51 +0800

    Easy_21_C
    208/208 cases passed (0 ms)
    Your runtime beats 100 % of c submissions
    Your memory usage beats 27.97 % of c submissions (6.2 MB)

--- 21.merge-two-sorted-lists.c
+++ 21.merge-two-sorted-lists.c
@@ -12,10 +12,22 @@
  *     struct ListNode *next;
  * };
  */
+typedef struct ListNode LINODE;
 
-
-struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    LINODE ans, *tmp = &ans;
+    while (list1 && list2) {
+        if (list1->val <= list2->val) {
+            tmp->next = list1;
+            list1 = list1->next;
+        } else {
+            tmp->next = list2;
+            list2 = list2->next;
+        }
+        tmp = tmp->next;
+    }
+    tmp->next = list1 ? list1 : list2;
+    return ans.next;
 }
 // @lc code=end
 
