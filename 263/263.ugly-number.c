14fbbbae828ca4cff78a1c774ca405e14d145ad1
Yuanster <124578ster@gmail.com>
2022-11-15 13:05:04 +0800

    Update 263 note

--- 263/263.ugly-number.c
+++ 263/263.ugly-number.c
@@ -1,12 +1,3 @@
-/*
- * @lc app=leetcode id=263 lang=c
- *
- * [263] Ugly Number
- */
-
-// @lc code=start
-
-
 bool isUgly(int n){
     if ((n == 1) || (n == 2) || (n == 3) || (n == 5))
         return true;
@@ -16,27 +7,3 @@ bool isUgly(int n){
     
     return false;
 }
-// @lc code=end
-
-/**
- * @brief An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
- * Given an integer n, return true if n is an ugly number.
-
- * Example 1:
- *  Input: n = 6
- *  Output: true
- *  Explanation: 6 = 2 × 3
-
- * Example 2:
- *  Input: n = 1
- *  Output: true
- *  Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5. 
-
- * Example 3:
- *  Input: n = 14
- *  Output: false
- *  Explanation: 14 is not ugly since it includes the prime factor 7.
-
- * Constraints:
- * -2^31 <= n <= 2^31 - 1
- */
--- 263/263.ugly-number.py
+++ 263/263.ugly-number.py
@@ -1,10 +1,3 @@
-#
-# @lc app=leetcode id=263 lang=python
-#
-# [263] Ugly Number
-#
-
-# @lc code=start
 from operator import truediv
 
 def divideBy(n, div):
@@ -30,27 +23,3 @@ class Solution(object):
         # return False
 
         return divideBy(divideBy(divideBy(n, 2), 3), 5) == 1
-
-# @lc code=end
-
-'''
-    An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
-    Given an integer n, return true if n is an ugly number.
-    Example 1:
-        Input: n = 6
-        Output: true
-        Explanation: 6 = 2 × 3
-
-    Example 2:
-        Input: n = 1
-        Output: true
-        Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5. 
-
-    Example 3:
-        Input: n = 14
-        Output: false
-        Explanation: 14 is not ugly since it includes the prime factor 7.
-
-    Constraints:
-        -2^31 <= n <= 2^31 - 1
-'''
