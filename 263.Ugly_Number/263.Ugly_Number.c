e7c9d875bc5d7aeb485a569b1e58a564ad3c63ab
Yuanster <124578ster@gmail.com>
2022-11-16 14:30:33 +0800

    Remove code files, they are not useful in website

--- 263.Ugly_Number/263.Ugly_Number.c
+++
@@ -1,9 +0,0 @@
-bool isUgly(int n){
-    if ((n == 1) || (n == 2) || (n == 3) || (n == 5))
-        return true;
-    for (int i = 2;i <= ceil(sqrt(n));i ++)
-        if(n%i == 0)
-            return isUgly(i) & isUgly(n / i);
-    
-    return false;
-}
