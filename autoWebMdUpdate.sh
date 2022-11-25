8eaf86880e6c3b7c3a8558e1254bccc26d48ae0a
Yuanster <124578ster@gmail.com>
2022-11-26 04:00:41 +0800

    Update little tip

--- autoWebMdUpdate.sh
+++ autoWebMdUpdate.sh
@@ -4,7 +4,7 @@ echo Updated folder is $1
 echo The script will update these file to GitHub page branch:
 out=$( ls $1 | grep ".*.md" )
 echo $out
-folder=$( echo `echo $1 | sed "s&.$&&"` )
+folder=$( echo `echo $1 | sed "s&\.$&&"` )
 # Change to GitHub branch
 #git checkout gh-pages
 # Update index file
