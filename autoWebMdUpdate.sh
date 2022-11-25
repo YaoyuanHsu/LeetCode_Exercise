c697f8fa74ff87edf8797f0893c068df7df3ea20
Yuanster <124578ster@gmail.com>
2022-11-26 03:58:37 +0800

    script test

--- autoWebMdUpdate.sh
+++ autoWebMdUpdate.sh
@@ -5,5 +5,16 @@ echo The script will update these file to GitHub page branch:
 out=$( ls $1 | grep ".*.md" )
 echo $out
 folder=$( echo `echo $1 | sed "s&.$&&"` )
-echo $folder$out
+# Change to GitHub branch
 #git checkout gh-pages
+# Update index file
+git checkout index.md
+git add index.md
+git commit -m "Update question list @ `date +'%Y/%m/%d'`"
+# Update new note
+git chekout master `echo $folder$out`
+mv `echo $folder$out` `echo ./pages/$out`
+git restore --staged `echo $folder$out`
+git add pages/$out
+git commit -m "Update note of $out @ `date +'%Y/%m/%d'`"
+git pull
