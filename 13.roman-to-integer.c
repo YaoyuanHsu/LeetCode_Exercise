0458af03c1f8c43025b3190bce3ea69294c97e1b
Yuanster <124578ster@gmail.com>
2022-12-05 20:54:02 +0800

    Remove comment

--- 13.roman-to-integer.c
+++ 13.roman-to-integer.c
@@ -1,21 +1,3 @@
-/*
- * @lc app=leetcode id=13 lang=c
- *
- * [13] Roman to Integer
- */
-
-// @lc code=start
-/*
- * Symbol       Value
- * I             1
- * V             5
- * X             10
- * L             50
- * C             100
- * D             500
- * M             1000
- */
-
 int romanToInt(char* s) {
     int ans = 0, length = strlen(s);
     int* strArray = calloc(length, sizeof(int));
@@ -64,4 +46,3 @@ int romanToInt(char* s) {
     }
     return ans;
 }
-// @lc code=end
