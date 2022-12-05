881ed26967018cd4e7e8f0abf54530ac07218802
Yuanster <124578ster@gmail.com>
2022-12-05 22:52:00 +0800

    Update note of 19

--- 19.Remove_Nth_Node_From_End_of_List/19.Remove_Nth_Node_From_End_of_List.c
+++ 19.Remove_Nth_Node_From_End_of_List/19.Remove_Nth_Node_From_End_of_List.c
@@ -1,18 +1,3 @@
-/*
- * @lc app=leetcode id=19 lang=c
- *
- * [19] Remove Nth Node From End of List
- */
-
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-
 typedef struct ListNode ListNode;
 
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
@@ -24,7 +9,7 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
         tmp = tmp->next;
 
     // Move the whole set to the end
-    for (; tmp->next != NULL;) {
+    for (; tmp->next != NULL ;) {
         tmp = tmp->next;
         last = preN;
         preN = preN->next;
@@ -46,27 +31,3 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
     free(tmp);
     return head;
 }
-// @lc code=end
-
-/**
- * @brief Given the head of a linked list, remove the n^th node from the end of
- * the list and return its head.
-
- * Example 1:
- *  Input: head = [1,2,3,4,5], n = 2
- *  Output: [1,2,3,5]
-
- * Example 2:
- *  Input: head = [1], n = 1
- *  Output: []
-
- * Example 3:
- *  Input: head = [1,2], n = 1
- *  Output: [1]
-
- * Constraints:
- *  The number of nodes in the list is sz.
- *  1 <= sz <= 30
- *  0 <= Node.val <= 100
- *  1 <= n <= sz
- */
