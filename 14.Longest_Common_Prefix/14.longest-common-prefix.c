e087af46878c2ac4ac84cf6eaaf69184c3e72ca7
Yuanster <124578ster@gmail.com>
2022-12-05 22:34:46 +0800

    Update note of 14

--- 14.Longest_Common_Prefix/14.longest-common-prefix.c
+++ 14.Longest_Common_Prefix/14.longest-common-prefix.c
@@ -1,16 +1,8 @@
-/*
- * @lc app=leetcode id=14 lang=c
- *
- * [14] Longest Common Prefix
- */
-
-// @lc code=start
-
 char* longestCommonPrefix(char** strs, int strsSize) {
     char compareChar;
     char *returnChar, *newPtr;
-    returnChar = (char*)malloc(
-        sizeof(char));   // size of char and size of char pointer is different
+    // size of char and size of char pointer is different
+    returnChar = (char*)malloc(sizeof(char));
     // initialize memory for empty prefix return
     memset(returnChar, '\0', sizeof(char));
     for (int charIndex = 0; strs[0][charIndex] != '\0'; charIndex++) {
@@ -29,29 +21,5 @@ char* longestCommonPrefix(char** strs, int strsSize) {
         strncpy(returnChar + charIndex + 1, "\0", 1);
     }
 end:
-
     return returnChar;
 }
-// @lc code=end
-
-/*
- * Longest Common Prefix
- *
- * Write a function to find the longest common prefix
- * string amongst an array of strings.
- * If there is no common prefix, return an empty string "".
- *
- * Example 1:
- * Input: strs = ["flower","flow","flight"]
- * Output: "fl"
- *
- * Example 2:
- * Input: strs = ["dog","racecar","car"]
- * Output: ""
- * Explanation: There is no common prefix among the input strings.
- *
- * Constraints:
- * 1 <= strs.length <= 200
- * 0 <= strs[i].length <= 200
- * strs[i] consists of only lowercase English letters.
- */
